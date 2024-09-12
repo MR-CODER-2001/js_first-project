beada86422b091dab36411f70661d3ce3590d7b4
MR-CODER-2001 <md.sarfraj002001@gmail.com>
2024-09-12 10:22:59 +0000

    datatype

---
+++ .devcontainer/data-type-mumary.js
@@ -0,0 +1,23 @@
+                      /** primitive data type 7 hote hai */
+
+// string > iske andar alphabet ata hai iske double cot kae andar likha jata hai  "Rriaaz" "India "
+
+//number > sike andar number ata hai our isko bina kisi cot ke andar likha jata hai  1258/588 .
+
+// Boolean > is andar true and falls ata hai   
+
+// Null > null ka ki iske andar koi bhi value nahi hai bikul empty hai 
+
+// undefined > kisi variable name de ke uska value nahi diya jata hai ato undefined ata hai.
+
+// symbol iska ek unique identify type hota hai .
+
+// bigint iska matab hota hai jab jyada number ka value add karna hai to is data type ka use karnege.
+
+                    /** non-primitive / Reference */
+
+ // array ye squar brackets ke andara likha jata hai  jaise array hero["Shaktimaan" ,"spiderman"]  iska data-type check karne ke baad function hota hai.
+  
+ // object ye curly brackets ke andar likha jata hai {name ="sarfra"......} iska data-type check karne ke baad function hota hai.
+
+ // function iska data type object function hota hai 
