8689adf018379cc67938a7e4c957b7f72fd3261a
MR-CODER-2001 <md.sarfraj002001@gmail.com>
2024-09-12 10:06:51 +0000

    caompsrision

---
+++ .devcontainer/comparision.js
@@ -0,0 +1,6 @@
+console.log(2 > 1);
+console.log(2 == 3);
+console.log(2 != 5);
+ 
+
+/** comparision karte time ye baat ka khayal rakhna hai ki dono value ka data ttpe same hona chahiye */
---
+++ .devcontainer/conversion-operation.js
@@ -0,0 +1,28 @@
+                            /* *********** Conversions in JavaScript mean converting from one data type to another. ***************************** */
+
+//let score = "33" /* values write without cot this mean data type is number/ and value write with double cot this is string */
+  
+//let score = null
+//console.log(typeof (score)); /* data is number */
+
+//let valueInNumber = Number(score)  /* This is coomand used to converting string to number */ 
+
+//console.log(valueInNumber);
+
+// notes
+// convert 
+// "33" easy contert to number
+// "33abs" it is not covert to numer input is NAN.
+// true convert to number and input is 1.
+// false convert to number and input is 0.
+
+
+/*******************************************operation *******************************/
+
+let value = "rani"
+let negvalue = " I love you baby"
+
+
+console.log(value+negvalue);
+
+
