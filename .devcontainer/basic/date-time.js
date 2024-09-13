a545c18031c8a10ec1d582360131351835af6547
MR-CODER-2001 <md.sarfraj002001@gmail.com>
2024-09-14 00:11:41 +0530

    date-time.js

---
+++ .devcontainer/basic/date-time.js
@@ -0,0 +1,21 @@
+
+//const age = new Date() is mahtod se current adte and time show hota hai.
+
+//console.log(age); // output 2024-09-13T18:22:10.763Z
+//console.log(age.toDateString()); // output Fri Sep 13 2024
+//console.log(age.toString()); // output Fri Sep 13 2024 23:53:22 GMT+0530 (India Standard Time)
+//console.log(age.toLocaleDateString()); // output Fri Sep 13 2024 23:54:01 GMT+0530 (India Standard Time)
+//console.log(age.toISOString()); // output 2024-09-13T18:24:47.917Z
+
+
+//const mydate =  new Date(2019, 6, 23 (+1)) // is mathod se hum jo chahe wo date nikal sakte hai. 
+
+const mydate = new Date(2019, 5, 23) 
+
+
+// console.log(mydate.getDate() ); 
+// console.log(mydate.getDay() );
+// console.log(mydate.getMonth() );
+// console.log(mydate.getSeconds() );
+// console.log(mydate.getMilliseconds() );
+
